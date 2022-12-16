bcb48422afca8ad06d4ddac941d1362a77d05f5e
AlexSherrow <sherrowa226@gmail.com>
2022-12-15 23:44:32 -0500

    files

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,29 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+	
+	//Get
+	//URI - /hello-world
+	//method - "Hello World"
+	@GetMapping(path = "/hello-world")
+	
+	public String helloWorld() {
+		return "Hello World!";
+	}
+	
+	//Hello-World-bean
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPath(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+
+}
