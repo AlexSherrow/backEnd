00c0cc13c01c7cbf8fd188905d926750c90ecd99
AlexSherrow <sherrowa226@gmail.com>
2022-12-16 14:27:59 -0500

    'latest'

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -5,9 +5,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 //Controller
 @RestController
+@CrossOrigin(origins="http://localhost:4200")
 public class HelloWorldController {
 	
 	//Get
